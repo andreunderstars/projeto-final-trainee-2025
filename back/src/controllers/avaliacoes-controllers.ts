3cb28b2bef27fe38c7692613d824092474a4f8a4
Davi Lorena <davilorena2016@gmail.com>
2025-07-28 20:03:41 -0300

    feat: adiciona metodo para cadastrar avaliacao para album

--- back/src/controllers/avaliacoes-controllers.ts
+++ back/src/controllers/avaliacoes-controllers.ts
@@ -4,8 +4,45 @@ import { z } from "zod"
 
 class AvaliacoesController {
  
+async create(req: Request, res: Response) {
+ 
+ const albumIdParamSchema = z.string().transform((val) => {
+        const num = parseInt(val, 10);
+        return isNaN(num) ? undefined : num;
+      })
+      .refine((val) => val !== undefined, {
+        message: "ID do álbum inválido. Deve ser um número inteiro.",
+      });
+ 
+    const bodySchema = z.object({
+        title: z.string().max(100).trim(),
+        date: z.string().max(10).trim(),
+        score: z.number().min(0).max(10, "A nota deve ser entre 0 e 10."),
+        comment: z.string().max(1500).trim(),
+    })
 
+     const parsedAlbumId = albumIdParamSchema.safeParse(req.params.albumId);
 
+      const albumId = parsedAlbumId.data;
+
+    const { title, date, score, comment } = bodySchema.parse(req.body)
+
+      const avaliacao = await prisma.avaliacao.create({
+        data: {
+            title,
+            date,
+            score,
+            comment,
+            album: {
+            connect: {
+              id: albumId, 
+            },
+        },
+    }
+      })
+      return res.status(201).json(avaliacao);
 }
+  }
+
 
 export { AvaliacoesController }
--- back/src/routes/avaliacoes-routes.ts
+++ back/src/routes/avaliacoes-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { AvaliacoesController } from "../controllers/avaliacoes-controllers"
 
-const avaliacoesRoutes = Router()
+const avaliacoesRoutes = Router({ mergeParams: true })
 
 const avaliacoesController = new AvaliacoesController()
+
+avaliacoesRoutes.post("/", avaliacoesController.create.bind(avaliacoesController))
 
 export { avaliacoesRoutes }
